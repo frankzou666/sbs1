a3b91fe1b9922c9b085f31f1cee914b778d9bd8f
frankzou666 <zoufrank@localhost>
2023-07-03 01:57:58 +0800

    test

--- src/main/java/com/example/SbsApplication.java
+++ src/main/java/com/example/SbsApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SbsApplication {
 
+	
+	//
 	public static void main(String[] args) {
 		SpringApplication.run(SbsApplication.class, args);
 	}
